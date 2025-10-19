740893bce4b1177209a605349f72636bad75a851
Frost-Marc <124355875+Frost-Marc@users.noreply.github.com>
2025-10-19 14:43:29 +0100

    added trail vfx to ball, creates trail based on the balls movement and removes the trail overtime.

--- Breakout/Ball.cpp
+++ Breakout/Ball.cpp
@@ -1,5 +1,6 @@
 #include "Ball.h"
 #include "GameManager.h" // avoid cicular dependencies
+#include <iostream>
 
 Ball::Ball(sf::RenderWindow* window, float velocity, GameManager* gameManager)
     : _window(window), _velocity(velocity), _gameManager(gameManager),
@@ -40,12 +41,14 @@ void Ball::update(float dt)
         _sprite.setFillColor(sf::Color(flicker, flicker / 2, 0)); // Orange flickering color
     }
 
-    // Update position with a subtle floating-point error
-    _sprite.move(_direction * _velocity * dt);
-
     // check bounds and bounce
     sf::Vector2f position = _sprite.getPosition();
     sf::Vector2u windowDimensions = _window->getSize();
+
+    // Update position with a subtle floating-point error
+    _sprite.move(_direction * _velocity * dt);
+
+    updateTrail(dt);
 
     // bounce on walls
     if ((position.x >= windowDimensions.x - 2 * RADIUS && _direction.x > 0) || (position.x <= 0 && _direction.x < 0))
@@ -57,14 +60,6 @@ void Ball::update(float dt)
     if (position.y <= 0 && _direction.y < 0)
     {
         _direction.y *= -1;
-    }
-
-    // lose life bounce
-    if (position.y > windowDimensions.y)
-    {
-        _sprite.setPosition(0, 300);
-        _direction = { 1, 1 };
-        _gameManager->loseLife();
     }
 
     // collision with paddle
@@ -90,10 +85,44 @@ void Ball::update(float dt)
     {
         _direction.y *= -1; // Bounce vertically
     }
+
+    // lose life bounce
+    if (position.y > windowDimensions.y)
+    {
+        _sprite.setPosition(0, 300);
+        _direction = { 1, 1 };
+
+        trailPos.clear();
+
+        _gameManager->loseLife();
+        
+        return;
+    }
+
 }
 
 void Ball::render()
 {
+    for (size_t i = 0; i < trailPos.size(); ++i)
+    {
+        float alphaFactor = 1.0f - static_cast<float>(i) / trailPos.size();
+        sf::Uint8 alpha = static_cast<sf::Uint8>(alphaFactor * 255);
+
+        sf::CircleShape trailDot(RADIUS * (0.75f + 0.25f * alphaFactor));
+        trailDot.setPosition(trailPos[i]);
+
+        if (_isFireBall)
+        {
+            trailDot.setFillColor(sf::Color(255, 100, 0, alpha));
+        }
+        else
+        {
+            trailDot.setFillColor(sf::Color(0, 255, 255, alpha));
+        }
+
+        _window->draw(trailDot);
+    }
+
     _window->draw(_sprite);
 }
 
@@ -113,4 +142,23 @@ void Ball::setFireBall(float duration)
     }
     _isFireBall = false;
     _timeWithPowerupEffect = 0.f;    
+}
+
+void Ball::updateTrail(float dt)
+{
+    //slows the removal of trail
+    trailTimer += dt;
+    if (trailTimer >= trailUpdateInterval)
+    {
+        //add current ball pos to the front
+        trailPos.push_front(_sprite.getPosition());
+
+        //limit the amount of balls in trail
+        if (trailPos.size() > trailLength)
+        {
+            trailPos.pop_back();
+        }
+
+        trailTimer = 0.f;
+    }
 }
